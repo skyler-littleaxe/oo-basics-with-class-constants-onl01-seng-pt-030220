a829259f55bf35f297002f20f0ee4dcbd35019b3
Skyler Littleaxe <littleaxe.skyler@gmail.com>
2020-03-19 22:45:59 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,6 +1,8 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
+  
+  BRANDS = []
 
   def initialize(brand)
     @brand = brand
@@ -11,5 +13,10 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
   
+  def brand=(brand)
+    @brand = brand
+    BRAND << brand
+  
+  end
 
 end
